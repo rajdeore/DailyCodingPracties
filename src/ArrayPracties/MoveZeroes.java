75cda7bde0c20e56a1a849053be235d9694abd65
raj <sanketrajvivek@gmail.com>
2023-12-04 18:10:51 +0530

    Move Zero

--- src/ArrayPracties/MoveZeroes.java
+++ src/ArrayPracties/MoveZeroes.java
@@ -17,8 +17,36 @@ public class MoveZeroes {
         return resultArray;
     }
 
+    public static int[] moveZeroesUsingTwoPointer(int[] givenArray){
+        if(givenArray.length == 1){
+            return givenArray;
+        }
+        int i = 0, j=1;
+        int len = givenArray.length;
+
+        while(i<len-1 && j<len){
+            if(givenArray[i] == 0 && givenArray[j] == 0){
+                j++;
+            } else if (givenArray[i] != 0 && givenArray[j] == 0) {
+                i++;
+            } else if (givenArray[i] == 0 && givenArray[j] != 0) {
+                int temp = givenArray[i];
+                givenArray[i] = givenArray[j];
+                givenArray[j] = temp;
+                i++;
+                j++;
+            }else{
+                i++;
+                j++;
+            }
+
+        }
+        return givenArray;
+    }
+
     public static void main(String[] args) {
         int[] givenArray = {0, 1, 0, 3, 12, 0};
-        System.out.println(Arrays.toString((moveZeroesUsingExtraArray(givenArray))));
+        //System.out.println(Arrays.toString((moveZeroesUsingExtraArray(givenArray))));
+        System.out.println(Arrays.toString(moveZeroesUsingTwoPointer(givenArray)));
     }
 }
