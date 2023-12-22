55e43e34cd68f7f6fd134660c89fc998e7a113a4
Raj Kumar Deore <raj.kumar.deore@oracle.com>
2023-12-22 20:21:41 +0530

    java concept

---
+++ src/JavaConceptPracties/rough/NullDemo.java
@@ -0,0 +1,11 @@
+package JavaConceptPracties.rough;
+
+public class NullDemo {
+
+
+    public static void main(String[] args) {
+        String password;
+
+       // System.out.println(password);
+    }
+}
