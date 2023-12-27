fa107319b044dcb1dce8a32b8689f7d9b5bc389b
Raj Kumar Deore <raj.kumar.deore@oracle.com>
2023-12-27 13:11:06 +0530

    equal and == diff

--- src/JavaConceptPracties/StringClassPracties/EqualsAndDoubleEquals.java
+++ src/JavaConceptPracties/StringClassPracties/EqualsAndDoubleEquals.java
@@ -1,6 +1,5 @@
 package JavaConceptPracties.StringClassPracties;
 
-import java.util.Objects;
 
 public class EqualsAndDoubleEquals {
 
@@ -12,18 +11,18 @@ public class EqualsAndDoubleEquals {
         String s3 = "modi";
         String s4 = "modi";
 
-        System.out.println( "s1 == s2 " +  s1 == s2);
-        System.out.println( "s1 == s3 " + (s1 == s3));
-        System.out.println(  "s3 == s4 " + (s3 == s4));
+        System.out.println("s1 == s2 " + s1 == s2);
+        System.out.println("s1 == s3 " + (s1 == s3));
+        System.out.println("s3 == s4 " + (s3 == s4));
 
 
-        System.out.println( "first : " +  (s1 == s2));
-        System.out.println( "second :" + (s1 == s3));
-        System.out.println(  "3rd : " + (s3 == s4));
+        System.out.println("first : " + (s1 == s2));
+        System.out.println("second :" + (s1 == s3));
+        System.out.println("3rd : " + (s3 == s4));
 
 
-        System.out.println( "s1 equals s2" +  s1.equals(s2));
-        System.out.println( "s1 equals s3" +  s1.equals(s3));
-        System.out.println( "s3 equals s4" +  s3.equals(s4));
+        System.out.println("s1 equals s2" + s1.equals(s2));
+        System.out.println("s1 equals s3" + s1.equals(s3));
+        System.out.println("s3 equals s4" + s3.equals(s4));
     }
 }
