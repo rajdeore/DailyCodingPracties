f85936fa5542bf2b8b7a23f0491e067dc2df1ecc
Raj Kumar Deore <raj.kumar.deore@oracle.com>
2024-01-08 10:23:08 +0530

    To print Array.

---
+++ src/ArrayPracties/ArrayPrint.java
@@ -0,0 +1,12 @@
+package ArrayPracties;
+public class ArrayPrint {
+    public static void arrayPrint(int[] givenArrray){
+        for (int i=0; i<givenArrray.length; i++) {
+            if(i != givenArrray.length-1){
+                System.out.print(givenArrray[i] + ", ");
+            }else{
+                System.out.print(givenArrray[i]);
+            }
+        }
+    }
+}
