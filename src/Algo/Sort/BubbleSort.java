65517a27d9b1f9440dd84b64c1be2a728a6c8ec6
Raj Kumar Deore <raj.kumar.deore@oracle.com>
2024-01-08 10:18:41 +0530

    Main, Test case added

--- src/Algo/Sort/BubbleSort.java
+++ src/Algo/Sort/BubbleSort.java
@@ -1,23 +1,25 @@
 package Algo.Sort;
-
+import ArrayPracties.ArrayPrint;
 public class BubbleSort {
-
-    public int[] bubbleSort(int[] arr){
+    static public void bubbleSort(int[] arr) {
         int arrLength = arr.length;
-
-        for(int i=0; i<arrLength; i++){
-            for(int j=0; j<arrLength-(i+1); j++){
+        for (int i = 0; i < arrLength; i++) {
+            for (int j = 0; j < arrLength - (i + 1); j++) {
                 //swap
-                if(arr[j] > arr[j+1]){
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-
-        return arr;
-
     }
 
+    public static void main(String[] args) {
+        //TC-1
+        int[] nums1 = {3, 2, 1, 5, 6, 4};
+        bubbleSort(nums1);
+        ArrayPrint.arrayPrint(nums1);
+
+    }
 }
